76795723b4e22b0d5de4c07438feb18c1f0f4e51
ogzA <akankan.oguz@gmail.com>
2026-07-25 01:05:38 +0200

    feat: add db

---
+++ db/db.js
@@ -0,0 +1,120 @@
+const dishes = [
+	// --- Burger & Sandwiches ---
+	{
+		id: 1,
+		category: "burger",
+		name: "Veggie mushroom black burger",
+		description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+		price: 16.9,
+		imageUrl: "./assets/img/dishes/veggie-mushroom-black-burger.jpg",
+		amount: 0,
+	},
+	{
+		id: 2,
+		category: "burger",
+		name: "All meat burger",
+		description:
+			"Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+		price: 15.9,
+		imageUrl: "./assets/img/dishes/all-meat-burger.jpg",
+		amount: 0,
+	},
+	{
+		id: 3,
+		category: "burger",
+		name: "Beef red burger",
+		description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+		price: 14.9,
+		imageUrl: "./assets/img/dishes/beef-red-burger.jpg",
+		amount: 0,
+	},
+	{
+		id: 4,
+		category: "burger",
+		name: "Big chicken burger",
+		description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+		price: 15.9,
+		imageUrl: "./assets/img/dishes/big-chicken-burger.jpg",
+		amount: 0,
+	},
+
+	// --- Pizza ---
+	{
+		id: 5,
+		category: "pizza",
+		name: "Pizza Margherita",
+		description: "Tomato sauce, Mozzarella",
+		price: 11.9,
+		imageUrl: "./assets/img/dishes/pizza-margherita.jpg",
+		amount: 0,
+	},
+	{
+		id: 6,
+		category: "pizza",
+		name: "Pizza Chorizo",
+		description: "Tomato slices, Mozzarella, Chorizo",
+		price: 13.9,
+		imageUrl: "./assets/img/dishes/pizza-chorizo.jpg",
+		amount: 0,
+	},
+	{
+		id: 7,
+		category: "pizza",
+		name: "Funghi",
+		description: "Red onion, Olives, Button mushrooms, Mozzarella",
+		price: 12.9,
+		imageUrl: "./assets/img/dishes/pizza-funghi.jpg",
+		amount: 0,
+	},
+	{
+		id: 8,
+		category: "pizza",
+		name: "Quattro Formaggi with Chicken",
+		description:
+			"Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+		price: 15.9,
+		imageUrl: "./assets/img/dishes/quattro-formaggi-with-chicken.jpg",
+		amount: 0,
+	},
+
+	// --- Salad ---
+	{
+		id: 9,
+		category: "salad",
+		name: "Warm beef arugula salad",
+		description:
+			"Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried tomatoes, Balsamic-vinegar dressing",
+		price: 16.9,
+		imageUrl: "./assets/img/dishes/warm-beef-arugula-salad.jpg",
+		amount: 0,
+	},
+	{
+		id: 10,
+		category: "salad",
+		name: "Mini green salad",
+		description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+		price: 7.9,
+		imageUrl: "./assets/img/dishes/mini-green-salad.jpg",
+		amount: 0,
+	},
+	{
+		id: 11,
+		category: "salad",
+		name: "Green salad with sea food",
+		description:
+			"Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+		price: 16.9,
+		imageUrl: "./assets/img/dishes/green-salad-with-sea-food.jpg",
+		amount: 0,
+	},
+	{
+		id: 12,
+		category: "salad",
+		name: "Vegan green salad with tofu",
+		description:
+			"Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+		price: 14.9,
+		imageUrl: "./assets/img/dishes/vegan-green-salad-with-tofu.jpg",
+		amount: 0,
+	},
+];
