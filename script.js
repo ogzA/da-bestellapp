15ff684467c66b0379dab52fdaf342ea739bb86e
ogzA <akankan.oguz@gmail.com>
2026-07-25 15:58:44 +0200

    plan: add plan in script.js and test dishes

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dishes rendern
+// Dishes nach Kategorien render und ausgeben lassen
+// Dishes 1x in den Warenkorb hinzufügen (Add to basket)
+// Dishes amount erhöhen (Plus Icon)
+// eventuell amount * price?
+// Dishes amount verringen (Minus Icon)
+// Dishes löschen (Papierkorb Icon)
+
+console.log(dishes);
